b83d6f9b5045c7767bd564b23ee50c97558b2c3d
logan <153212028@qq.com>
2018-04-09 23:11:53 +0800

    。

---
+++ Dynamic Programming/dp5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/***
+ * 京东编程题 2018/4/9
+ * 一个字符串去除某些位的字符后会变成回文串
+ * 求方案个数
+ */
+public class dp5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        //暂时还没想清楚解题思路，先占个坑
+
+    }
+}
